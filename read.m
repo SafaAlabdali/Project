95dae8e99ace805149ac811917b9d711ce5b65e4
DESKTOP-P3OT4UA\Safa's PC <sanaa20@bath.ac.uk>
2020-07-19 16:10:11 +0400

    no message

--- read.m
+++ read.m
@@ -1,21 +1,21 @@
 %read 
 function [A,rhs,na]=read()
-fileID = fopen('dumpMatrix000.ymat','r');
+matfile = fopen('dumpMatrix000.ymat','r');
 % Read record 1: 3 32bit integers
-data = fread(fileID,3,'int32');
-ns = data(1);        % system size 
+data = fread(matfile,3,'int32');
+ns = data(1);         % system size 
 na = data(2);         % number of diagonal entries 
-nija = data(3);      %
+nija = data(3);       % size of matrix values val and row pointers/column indices ija
 nnz = nija - (na+1);  % number of ( non-zero off-diagonal )entries 
-fprintf('na = %d nija = %d nnz = %d\n',na,nija,nnz);
+
 % Read record 2: nija 32bit integers
-rowPtr = fread(fileID,na+1,'int32');
-colIdx = fread(fileID,nnz,'int32');
+rowPtr = fread(matfile,na+1,'int32');%Row pointers, length=number of rows (ie. number of diagonal entries) + 1
+colIdx = fread(matfile,nnz,'int32'); %Column indices, length=total number of non-zero entries=na+nnz
 % Read record 3: nija-1 64bit doubles
-values = fread(fileID,nija,'double');
+values = fread(matfile,nija,'double');
 % Read record 4: na 64bit doubles
-rhs = fread(fileID,na,'double');
-fclose(fileID);
+rhs = fread(matfile,na,'double');
+fclose(matfile);
 A = diag(values(1:na));
 for i=1:na
 for k=rowPtr(i):rowPtr(i+1)-1
