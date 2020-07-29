1ecf42920ee6e395f98afdd123dd693146ef322b
DESKTOP-P3OT4UA\Safa's PC <sanaa20@bath.ac.uk>
2020-07-29 17:13:22 +0400

    Error loop is added. This function used to test matrix called by GraphMatA.m.

--- Jacobi_D.m
+++ Jacobi_D.m
@@ -2,17 +2,19 @@
 % This program test Jacobi Method using diagonal matrix DT 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [res,k]=Jacobi_D(A,b,N)
+function [r,e,k]=Jacobi_D(A,b,N)
 
 D=diag(diag(A)); % diagonal matrix 
 u0=zeros(N,1);   % initial vector 
-  
+
+%%%%%%%%%%%%%%%%%
 res_0=norm(A*u0-b); % initial residual 
 res = res_0;
+%%%%%%%%%%%%%%%%%%%%%
 k=1;
-while ( (res(k)/res_0) > 1E-1)
+while ( (res(k)/res_0) > 1E-4)
     r=b-A*u0;
-    uk=u0+ D\r;         % jacobi(D,r)
+    uk=u0+ D\r;         % jacobi(T,r)
     res_k=norm(A*uk-b);
     u0=uk;
     res= [res res_k];   %vector that saves the residuals 
@@ -21,5 +23,21 @@ while ( (res(k)/res_0) > 1E-1)
         break;
     end 
 end
-res=res/res_0;
+
+%%%%%%%%%%%%%%%%%%%%%%
+u0=zeros(N,1);
+% This part will be done to test error 
+u_ex=rand(N,1);
+b_ex= A*u_ex;
+e0= norm(u_ex -u0);
+
+for i=1:k
+    r =b_ex-A*u0;
+    uk=u0+ D\r; %jacobi(D,r);
+    e(i)=norm(uk-u_ex);
+    u0=uk;
+end
+
+e =e/e0;
+r=res/res_0;
 end 
--- Jacobi_T.m
+++ Jacobi_T.m
@@ -2,7 +2,7 @@
 % This program test Jacobi Method using diagonal matrix DT 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [res,k]=Jacobi_D(A,b,N)
+function [r,e,k]=Jacobi_T(A,b,N)
 
 % construct tridiagonal mat 
 T = zeros(N,N);
@@ -11,7 +11,7 @@ T(N+1:1+N:N*N) = diag(A,1);
 T(2:1+N:N*N-N) = diag(A,-1);
 
 u0=zeros(N,1);   % initial vector 
-  
+
 
 res_0=norm(A*u0-b); % initial residual 
 res = res_0;
@@ -23,9 +23,25 @@ while ( (res(k)/res_0) > 1E-4)
     u0=uk;
     res= [res res_k];   %vector that saves the residuals 
     k=k+1;
-    if(k==100)
+    if(k==2)
         break;
     end 
 end
-res=res/res_0;
+%%%%%%%%%%%%%%%%%%%%%%
+u0=zeros(N,1);
+% This part will be done to find the error 
+u_ex=rand(N,1);
+b_ex= A*u_ex;
+e0= norm(u_ex -u0);
+
+%%%%%%%%%%%%%%%%%
+
+for i=1:k
+    r =b_ex-A*u0;
+    uk=u0+ T\r; %jacobi(T,r);
+    e(i)=norm(uk-u_ex);
+    u0=uk;
+end
+e =e/e0;
+r=res/res_0;
 end 
